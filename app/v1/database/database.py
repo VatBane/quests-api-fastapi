5176c9dc4cb5d1a654206584ef9ec9b623669b51
VatBane <mrkross35@gmail.com>
2025-03-29 16:06:35 +0200

    created db schema and migrations

---
+++ app/v1/database/database.py
@@ -0,0 +1,11 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+
+from v1.database.config import DBSettings
+
+
+engine = create_async_engine(DBSettings().url)
+
+
+async def get_session():
+    with AsyncSession(engine) as session:
+        yield session
